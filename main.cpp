57486e74bc6c7c0b90435c4b995c7ef3d8735b16
ROHIT <35218274+rohitbukan@users.noreply.github.com>
2020-10-08 22:48:19 +0530

    Linear queue

--- main.cpp
+++ main.cpp
@@ -1,75 +1,107 @@
-/*WELLNESS*/
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include<ostream>
+
 using namespace std;
-
-char stack[20];
-int MaxSize, tos;
-
-void initStack() {
- MaxSize = 20;
- tos = -1;
+int Q[100],maxsize,rear,front;
+void Createqueue(int size)
+{
+    maxsize=size;
+   front=0;
+rear=-1;
+}
+void Enqueue(int e)
+{
+    rear++;
+    Q[rear]=e;
+}
+int Dequeue()
+{
+    int temp;
+    temp=Q[front];
+    front++;
+    return (temp);
 }
 
-void push(char c) {
- stack[++tos] = c;
+int isFull()
+{
+    if (rear==(maxsize-1))
+    return(1);
+    else
+        return (0);
+}
+int isEmpty()
+{
+    if(front>rear)
+        return(1);
+        else
+        return(0);
+}
+void printQueue()
+{
+    int i;
+    for(i=front;i<=rear;i++)
+    {
+        cout<<Q[i]<<" ";
+    }
 }
 
-int isFull() {
- if (tos == MaxSize - 1) {
-  return 1;
- }
- else return 0;
+int main()
+{
+     int size;
+    int ch;
+    int num;
+    cout<<"Enter size of Queue"<<endl;
+    cin>>size;
+    Createqueue(size);
+     do{
+    cout << "Select choice\n 1. Enqueue \n 2. Dequeue \n 3. isFull \n 4. isEmpty \n 5. printQueue \n 0. Exit " << endl;
+    cin>>ch;
+    switch(ch){
+            case 1: if(isFull()){
+                    cout<<"Queue is full";
+
+
+                    }
+                    else
+                        {
+                            cout<<"Enter element"<<endl;
+                        cin>>num;
+                        Enqueue(num);
+                    }
+                    break;
+            case 2: if(isEmpty()){
+                cout<<"Queue is Empty";
+
+
+                    }
+                    else
+                        Dequeue();
+                    break;
+            case 3: if(isFull())
+                        cout<<"Full"<<endl;
+                    else
+                        cout<<"Not Full"<<endl;
+                    break;
+            case 4: if(isEmpty())
+                        cout<<"queue is empty"<<endl;
+                    else
+                        cout<<"Not empty"<<endl;
+                    break;
+            case 5:
+                if(isEmpty()){
+                cout<<"Queue is empty "<<endl;
+                }
+                else
+                    printQueue();
+                break;
+            case 0: exit(0);
+                    break;
+            default:
+                    cout<<"Wrong choice"<<endl;
+                    }
+     }
+    while(ch!=0);
+
+    return 0;
 }
 
-int isEmpty() {
- if (tos == -1) {
-  return 1;
- }
- else return 0;
-}
-
-char pop() {
- return stack[tos--];
-}
-
-char atTop() {
- return stack[tos];
-}
-
-void printStack() {
- for (int i = tos; i > -1; i--) {
-  cout << stack[i] << "\t";
- }
- cout << endl;
-}
-
-
-
-int main() {
- char exp[20];
- initStack();
-
- cout << "Enter the Expression"<<endl;
- cin >> exp;
- cout << "Enter expression size  "<<strlen(exp) << endl;
- for (int i = 0; i < strlen(exp); i++) {
-  if (exp[i] == '{') {
-   push(exp[i]);
-  }
-  else if (exp[i] == '}') {
-   if (pop() != '{') {
-    cout << "Expression not well formed";
-    exit(0);
-   }
-  }
- }
-
- if (isEmpty() == 1) {
-  cout << "Well formed expression";
- }
-
- else cout << "Expression is not well formed";
- return 0;
-
-}
